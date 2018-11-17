ecc8a525cf91b4144a19e6401063fc482d96b5bb
KaiKai <1035759181@qq.com>
2018-11-17 22:57:56 +0800

    这是第二次提交node脚本文件

--- jsdemo.js
+++ jsdemo.js
@@ -1,14 +1,18 @@
 var fs = require('fs')
-
-var dirName = process.argv[2] // 你传的参数是从第 2 个开始的
-
-fs.mkdirSync("./" + dirName) // mkdir $1
-process.chdir("./" + dirName) // cd $1
-fs.mkdirSync('css') // mkdir css
-fs.mkdirSync('js') // mkdir js
-
-fs.writeFileSync("./index.html", " <!DOCTYPE><title>Hello</title><h1>Hi</h1>")
-fs.writeFileSync("css/style.css", " h1{color: red;}")
-fs.writeFileSync("./js/main.js", "var string = 'Hello World' alert(string)")
-
+var dirname = process.argv[2]
+var path = "./" + dirname
+if(fs.existsSync(path))
+{
+    console.log('existence address')
+    process.exit(1)
+}
+else{
+fs.mkdirSync("./" + dirname)
+process.chdir("./" + dirname)
+fs.mkdirSync('css')
+fs.mkdirSync('js')
+fs.writeFileSync("./index.html"," <!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
+fs.writeFileSync("./css/style.css","h1{color: red;}")
+fs.writeFileSync("./js/main.js",'var string = "Hello World"\nalert(string)')
 process.exit(0)
+}
